6241c63ead1b8ed79b04dae1c24cd597fddcd3c0
Evan Longwood <evanmlongwood@gmail.com>
2018-10-30 14:40:51 -0700

    Ternary Operators, Logical and Operators, if statements, code challenge.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,14 +4,32 @@ console.log("App.js is running!");
 
 // JSX - Javascript XML
 
+// Going to use
+// if statements
+// ternary operators
+// logical and operator
+
 // create app object title/subtitle
 // user title/subtitle in the template
 // render template
 
+
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "Here are your options" "No options."
+
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer.'
+    subtitle: 'Put your life in the hands of a computer.',
+    options: ['One', 'Two']
 };
+
+// This works too.
+// function getSubtitle(subtitle) {
+//     if (subtitle) {
+//         return <p>{subtitle}</p>;
+//     }
+// }
+// getSubtitle(app.subtitle)
 
 var template = React.createElement(
     'div',
@@ -21,10 +39,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? "Here are your options." : "No options."
     ),
     React.createElement(
         'ol',
@@ -48,11 +71,23 @@ var template = React.createElement(
 // p -> Age:
 // p -> Location
 // RenderTwo instead of Template
+
 var user = {
     name: 'Andrew',
-    age: 26,
+    age: 19,
     location: 'Philadelphia'
 };
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 
 var templateTwo = React.createElement(
     'div',
@@ -60,19 +95,15 @@ var templateTwo = React.createElement(
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        user.location
-    )
+    getLocation(user.location)
 );
 var appRoot = document.getElementById('app');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,19 +2,38 @@ console.log("App.js is running!");
 
 // JSX - Javascript XML
 
+// Going to use
+// if statements
+// ternary operators
+// logical and operator
+
 // create app object title/subtitle
 // user title/subtitle in the template
 // render template
 
+
+// only render the subtitle (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "Here are your options" "No options."
+
 var app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer.'
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer.',
+    options: ['One', 'Two'],
 };
+
+// This works too.
+// function getSubtitle(subtitle) {
+//     if (subtitle) {
+//         return <p>{subtitle}</p>;
+//     }
+// }
+// getSubtitle(app.subtitle)
 
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are your options."  : "No options."}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -22,23 +41,31 @@ var template = (
     </div>
 );
 
+
 // Create a templateTwo var JSX Expression.
 // div
 // h1 -> Your name
 // p -> Age:
 // p -> Location
 // RenderTwo instead of Template
+
 var user = {
     name: 'Andrew',
-    age: 26,
+    age: 19,
     location: 'Philadelphia',
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
 }
 
 var templateTwo = (
   <div>
-      <h1>{user.name}</h1>
-      <p>Age: {user.age}</p>
-      <p>{user.location}</p>
+      <h1>{user.name ? user.name : 'Anonymous'}</h1>
+      {(user.age >= 18) && <p>Age: {user.age}</p>}
+      {getLocation(user.location)}
   </div>
 );
 var appRoot = document.getElementById('app');
