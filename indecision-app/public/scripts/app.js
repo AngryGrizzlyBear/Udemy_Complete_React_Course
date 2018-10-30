1fca5209671d82b29af782ae62d984dce81b097d
Evan Longwood <evanmlongwood@gmail.com>
2018-10-29 22:04:34 -0700

    JSX Challenge.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,10 +5,60 @@ console.log("App.js is running!");
 // JSX - Javascript XML
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+// Create a templateTwo var JSX Expression.
+// div
+// h1 -> Your name
+// p -> Age:
+// p -> Location
+// RenderTwo instead of Template
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "One Angry Grizzly Bear"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "29"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Somewhere in the desert of Nevada"
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,31 @@ console.log("App.js is running!");
 
 // JSX - Javascript XML
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+// Create a templateTwo var JSX Expression.
+// div
+// h1 -> Your name
+// p -> Age:
+// p -> Location
+// RenderTwo instead of Template
+
+var templateTwo = (
+  <div>
+      <h1>One Angry Grizzly Bear</h1>
+      <p>29</p>
+      <p>Somewhere in the desert of Nevada</p>
+  </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
