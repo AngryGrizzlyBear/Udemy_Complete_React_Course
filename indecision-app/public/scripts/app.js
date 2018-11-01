92506ab513fdb757edc55c5170386d10a8259a94
Evan Longwood <evanmlongwood@gmail.com>
2018-11-01 13:57:36 -0700

    Picking an option (and disabling a button when there's nothing in the list)

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -25,6 +25,13 @@ var onRemoveAll = function onRemoveAll() {
     render();
 };
 
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+    console.log(randomNum);
+};
+
 // Create remove all button above list
 // on click -> wipe the array -> rerender
 
@@ -53,9 +60,9 @@ var render = function render() {
             app.options.length > 0 ? "Here are your options." : "No options."
         ),
         React.createElement(
-            'p',
-            null,
-            app.options.length
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
