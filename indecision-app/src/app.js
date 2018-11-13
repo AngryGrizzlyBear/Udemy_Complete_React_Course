75a80e1f19c8e885f729e6748bc01ba4eac06955
Evan Longwood <evanmlongwood@gmail.com>
2018-11-13 13:16:45 -0800

    Lifecycle methods.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -24,6 +24,30 @@
             options: props.options
         };
     }
+
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({options: options}));
+            }
+        } catch(e) {
+            // Do nothing at all.
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
     handleDeleteOptions() {
         this.setState(() => ({options: [] }));
     }
@@ -140,6 +164,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p> }
             {
                 props.options.map((option) => (
                     <Option
@@ -210,6 +235,10 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
         this.setState(() => ({error}));
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
     }
     render() {
         return (
