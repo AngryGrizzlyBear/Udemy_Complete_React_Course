3a3012c97edb72d1e6bdebb5de38f1f79f30bd77
Evan Longwood <evanmlongwood@gmail.com>
2018-11-14 08:35:40 -0800

    Working with importing react and react dom for the first time.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,19 +1,6 @@
-import isSenior, { isAdult, canDrink } from "./person";
-console.log(isAdult(17));
-console.log(canDrink(21));
-console.log(isSenior(64));
-// import subtract, { square, add } from './utils.js';
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
+// install -> import -> use
+import React from 'react';
+import ReactDom from 'react-dom';
 
-
-// make person.js
-// named export isAdult(18) = true if adult, otherwise false.
-// named export canDrink(18) - true if 21 and over, otherwise false.
-
-// import isAdult and canDrink
-// use both printing result to the console.
-// Setting up the default export and function
-// Grab the default and call it
+const template = React.createElement('p', {}, 'testing 123');
+ReactDom.render(template, document.getElementById('app'))
