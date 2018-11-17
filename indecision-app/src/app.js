84f4282fb5deb8d86abcf6a77c7b2435c35aadc4
Evan Longwood <evanmlongwood@gmail.com>
2018-11-17 10:40:13 -0800

    refactored

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 
-const Layout = (props) => {
-    return (
-      <div>
-          <p>header</p>
-          {props.children}
-          <p>footer</p>
-      </div>
-    );
-};
-
-const template = (
-    <div>
-        <h1>Page Title</h1>
-        <p>This is my page.</p>
-    </div>
-);
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
