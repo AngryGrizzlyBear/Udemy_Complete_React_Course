8d37a79433e2729be8a217c6b54640424debb77c
Evan Longwood <evanmlongwood@gmail.com>
2018-11-19 13:34:05 -0800

    updated the background page, colors and everything else.

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -83,20 +83,22 @@ export default class IndecisionApp extends React.Component {
         const subtitle = 'Put your life in the hands of a computer.';
 
         return (
-            <div>
+            <div>der
                 <Header subtitle={subtitle} />
-                <Action
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <Options
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption}
+                    />
+                    <AddOption
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
