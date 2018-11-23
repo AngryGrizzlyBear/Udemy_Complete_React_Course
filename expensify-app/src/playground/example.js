be22fcd536bd508836c359c2222071bc2bca610a
Evan Longwood <evanmlongwood@gmail.com>
2018-11-23 14:10:59 -0800

    Linking between Routes. Learned about navlinks.

---
+++ expensify-app/src/playground/example.js
@@ -0,0 +1,47 @@
+
+// // link to the create expense page
+// const Create = () => (
+//     <div>
+//          <Link to="/create">Create</Link>
+//     </div>
+// );
+
+// const Homepage = () => (
+//     <div>
+//         <Link to="/">Homepage </Link>
+//     </div>
+// );
+
+// // link to the edit page
+// const Edit = () => (
+//     <div>
+//         <Link to="/edit">Edit</Link>
+//     </div>
+// );
+// // linking to the help page.
+// const Help = () => (
+//     <div>
+//          <Link to="/help">Help</Link>
+//     </div>
+// );
+//
+// import {BrowserRouter, Route, Switch} from "react-router-dom";
+// import React from "react";
+//
+// BrowserRouter>
+// <div>
+// <Header />
+// <Homepage />
+// <Create />
+// <Edit />
+// <Help />
+// <Switch>
+// <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+// <Route path="/create" component={AddExpensePage} />
+// <Route path="/edit" component={EditExpensePage} />
+// <Route path="/help" component={HelpPage} />
+// <Route component={NotFoundPage}/>
+// </Switch>
+//
+// </div>
+// </BrowserRouter>
