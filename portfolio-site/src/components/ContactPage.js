cbafd4b54b72ca090960d36f552d990ca3368c98
Evan Longwood <evanmlongwood@gmail.com>
2018-11-23 17:04:16 -0800

    Updated the Contact Page.

--- portfolio-site/src/components/ContactPage.js
+++ portfolio-site/src/components/ContactPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const ContactPage = () => (
-  <div>This is my Contact Page.</div>
+  <div>
+      <h1>Contact Me</h1>
+      <p>You can reach me at test@gmail.com</p>
+  </div>
 );
 
 
