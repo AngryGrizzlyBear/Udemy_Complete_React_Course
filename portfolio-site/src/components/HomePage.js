51e80a750723ec34c960b5d8b8f592b165bf8216
Evan Longwood <evanmlongwood@gmail.com>
2018-11-23 17:02:54 -0800

    Updated the Homepage.

--- portfolio-site/src/components/HomePage.js
+++ portfolio-site/src/components/HomePage.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const HomePage = () => (
   <div>
-      This is my Homepage.
+      <p>This is my Homepage.</p>
+      <h1>Welcome!</h1>
+      <p>This is my site. Take a look around!</p>
   </div>
 );
 
