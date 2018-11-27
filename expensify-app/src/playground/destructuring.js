5d03b485424e113affa9c48595717c87b5ca9826
Evan Longwood <evanmlongwood@gmail.com>
2018-11-27 11:12:57 -0800

    Array Destructuring.

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// object destructuring
+
 // const person = {
 //     name: 'Andrew',
 //     age: 27,
@@ -17,13 +19,29 @@
 // if (city && temperature) {
 //     console.log(`It's ${temperature} in ${city}`)
 // }
+//
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name:'Penguin'
+//     }
+// };
+// const {name: publisherName = 'Nobody'} = book.publisher;
+// console.log(publisherName);
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name:'Penguin'
-    }
-};
-const {name: publisherName = 'Nobody'} = book.publisher;
-console.log(publisherName);
+
+//
+//Array Destructuring
+//
+
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [ , city, state] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$2.00', '$3.50', '$2.75'];
+const [ hot , , price] = item;
+// grab first and third items using array destructuring
+
+console.log(`A medium ${hot} costs ${price}`);
