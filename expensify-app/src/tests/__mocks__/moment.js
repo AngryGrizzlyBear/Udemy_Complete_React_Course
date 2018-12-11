c052ed38f7c1a9b6be378a00c3f324480273ec58
Evan Longwood <evanmlongwood@gmail.com>
2018-12-11 11:33:32 -0800

    Mocking Libraries with Jest.

---
+++ expensify-app/src/tests/__mocks__/moment.js
@@ -0,0 +1,8 @@
+// import moment from 'moment';
+const moment = require.requireActual('moment');
+
+export default (timestamp = 0) => {
+ return moment(timestamp);
+};
+
+// should render ExpenseForm with expense data
